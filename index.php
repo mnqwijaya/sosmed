b378aa7b4f415eca746eb2d7b753789aee59e226
nurqomari <nurqomari@gmail.com>
2022-01-30 15:32:02 +0000

    perubahan index php

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>Selamat datang di Pesbuk</h1>
-                        <p>Bergabunglah bersama jutaan orang lainnya...</p>
+                        <h1>Selamat datang di Buku Wajah Tahunan</h1>
+                        <p>Bergabunglah bersama jutaan orang lainnya...bersama Nur Qomari</p>
                     </div>
                     <div class="col-md-4">
                         <a href="login.php" class="btn btn-secondary">Masuk</a>
